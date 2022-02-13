a7622990de7f262d37259283819ef8e7af601d63
Matej Marjanovic <matej.marjanovich@gmail.com>
2022-02-13 16:11:41 +0100

    working first version

--- dist/code.js
+++ dist/code.js
@@ -1,36 +1,127 @@
+const SPIRAL_CONSTANTS = {
+    SPIRAL_TYPE_ARCHIMEDEAN: 0,
+    SPIRAL_TYPE_LOGARITHIMIC: 1
+};
 // This plugin will open a modal to prompt the user to enter a number, and
 // it will then create that many of the chose shape on screen
 // This shows the HTML page in "index.html".
-figma.showUI(__html__, { width: 232, height: 216 });
+figma.showUI(__html__, { width: 400, height: 620 });
+let svgPath = false;
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
-figma.ui.onmessage = msg => {
+figma.ui.onmessage = (msg) => {
     // One way of distinguishing between different types of messages sent from
     // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-shapes') {
-        const nodes = [];
-        for (let i = 0; i < msg.count; i++) {
-            var shape;
-            if (msg.shape === 'rectangle') {
-                shape = figma.createRectangle();
-                console.log('hello');
-            }
-            else if (msg.shape === 'triangle') {
-                shape = figma.createPolygon();
-            }
-            else {
-                shape = figma.createEllipse();
-            }
-            shape.x = i * 150;
-            shape.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-            figma.currentPage.appendChild(shape);
-            nodes.push(shape);
+    if (msg.type === "create-shapes" || msg.type === "create-spiral") {
+        const pathDAttribute = makeSpiralPoints(msg.type === "create-shapes" ?
+            { innerRadius: 10,
+                outerRadius: 100,
+                degrees: 720,
+                points: 48,
+                lineWidth: 1,
+                shouldMakeHelix: false,
+                shouldAdjustHelixHeight: false,
+                helixOffsetX: 0,
+                helixOffsetY: 0,
+                helixHWRatio: 0,
+                helixIsoAngle: 0
+            } : msg.data);
+        if (svgPath) {
+            svgPath.remove();
         }
-        figma.currentPage.selection = nodes;
-        figma.viewport.scrollAndZoomIntoView(nodes);
+        svgPath = figma.createNodeFromSvg(`<svg xmlns="http://www.w3.org/2000/svg"><path d="${pathDAttribute}" fill="none" stroke="black" stroke-width="${msg.data.lineWidth}px"/> </svg>`);
+        if (figma.currentPage.selection.length > 0) {
+            const n0 = figma.currentPage.selection[0];
+            const spiralXOffset = svgPath.x;
+            const spiralYOffset = svgPath.y;
+            const centerX = n0.x + n0.width / 2 + spiralXOffset;
+            const centerY = n0.y + n0.height / 2 + spiralYOffset;
+            svgPath.x = centerX;
+            svgPath.y = centerY;
+        }
+        //   figma.currentPage.appendChild(svgPath);
     }
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
-    figma.closePlugin();
+    else if (msg.type === "close" || msg.type === "done") {
+        figma.closePlugin();
+    }
 };
+function makeSpiralPoints(data) {
+    const currentSpiralType = parseInt(data.currentSpiralType);
+    let innerR = Math.max(parseInt(data.innerRadius), 0);
+    if (currentSpiralType === SPIRAL_CONSTANTS.SPIRAL_TYPE_LOGARITHIMIC) {
+        innerR = 1;
+    }
+    let outerR = Math.max(parseInt(data.outerRadius), 1.0);
+    // Inner and Outer Radius also can't both be 0 for archimedean spiral.
+    // Setting both to 0 erases the shape and plugin needs to be restarted.
+    // Forcing outerR to 1.0 if both InnerR and outerR are set to 0 to prevent this issue.
+    if (innerR == 0 && outerR == 0) {
+        outerR = 1.0;
+    }
+    const degrees = Math.max(parseInt(data.degrees), 1);
+    const points = Math.max(parseInt(data.points), 2);
+    const numQCoordinates = points * 2;
+    const lineWidth = parseFloat(data.lineWidth);
+    const shouldMakeHelix = data.shouldMakeHelix;
+    const shouldAdjustHelixHeight = data.shouldAdjustHelixHeight;
+    const helixOffsetX = parseFloat(data.helixOffsetX);
+    let helixOffsetY = parseFloat(data.helixOffsetY);
+    const helixHWRatio = parseFloat(data.helixHWRatio);
+    const helixIsoAngle = parseFloat(data.helixIsoAngle);
+    if (shouldAdjustHelixHeight) {
+        helixOffsetY = helixOffsetY * Math.sin(helixIsoAngle * (Math.PI / 180));
+    }
+    const helixPointOffsetX = helixOffsetX / points;
+    const helixPointOffsetY = helixOffsetY / points;
+    const pointDistanceIncrement = (outerR - innerR) / (numQCoordinates);
+    const pointsArr = [];
+    // two x, y pairs are needed to define one point on the Q bezier curve.
+    if (currentSpiralType == SPIRAL_CONSTANTS.SPIRAL_TYPE_ARCHIMEDEAN) {
+        for (let i = 0; i <= numQCoordinates; i++) {
+            const bezierCoordinateLength = innerR + i * pointDistanceIncrement;
+            // var zigZag = false;
+            // var lengthChange = 0;
+            // if (zigZag) {
+            //   var lengthChange = pointLength / 10;
+            //   if (i % 2 == 1) {
+            //     lengthChange = -pointLength / 10;
+            //   }
+            // }
+            // var centerToPointLength = Math.sqrt(pointX*pointX+pointY*pointY); // length is defined.
+            // var centerToPointNormalVectorX = pointX / centerToPointLength;
+            // var centerToPointNormalVectorY = pointY / centerToPointLength;
+            const bezierCoordinateAngle = i * (degrees / (numQCoordinates));
+            let pointX = bezierCoordinateLength * Math.cos(bezierCoordinateAngle * (Math.PI / 180));
+            let pointY = bezierCoordinateLength * Math.sin(bezierCoordinateAngle * (Math.PI / 180));
+            if (shouldMakeHelix) {
+                pointY = pointY * helixHWRatio;
+                pointY = pointY + i * helixPointOffsetY;
+                pointX = pointX + i * helixPointOffsetX;
+            }
+            pointsArr.push(pointX + " " + pointY);
+        }
+        return "M " + pointsArr.shift() + " Q " + pointsArr.join(" ");
+    }
+    else if (currentSpiralType == SPIRAL_CONSTANTS.SPIRAL_TYPE_LOGARITHIMIC) {
+        // For Log Spiral, inner radius can't be 0 (log spiral only approaches, never reaches 0). 
+        const a_log = innerR;
+        const b_log = (Math.log(outerR / innerR)) / (2 * Math.PI * (degrees / 360.0));
+        const degrees_per_point_log = degrees / numQCoordinates;
+        const radians_per_point_log = degrees_per_point_log * (Math.PI / 180);
+        for (var i = 0; i <= numQCoordinates; i++) {
+            const pointAngleDeg = i * degrees_per_point_log;
+            const pointAngleRad = i * radians_per_point_log;
+            const pointLength = a_log * Math.pow(Math.E, b_log * pointAngleRad);
+            let pointX = pointLength * Math.cos(pointAngleDeg * (Math.PI / 180));
+            let pointY = pointLength * Math.sin(pointAngleDeg * (Math.PI / 180));
+            if (shouldMakeHelix) {
+                pointY = pointY * helixHWRatio;
+                pointY = pointY + i * helixPointOffsetY;
+                pointX = pointX + i * helixPointOffsetX;
+            }
+            pointsArr.push(pointX + " " + pointY);
+        }
+        return "M " + pointsArr.shift() + " Q " + pointsArr.join(" ");
+    }
+}
--- src/ui/scripts/scripts.js
+++ src/ui/scripts/scripts.js
@@ -1,31 +1,33 @@
 //vars
-const createShapesButton = document.querySelector('#createShapes');
-const cancelButton = document.querySelector('#cancel');
-const shapeMenu = document.querySelector('#shape');
-const countInput = document.querySelector('#count');
+const updateSpiralButton = document.querySelector('#updateSpiralButton');
+// const cancelButton = document.querySelector('#cancel');
 
 //on load function
 document.addEventListener("DOMContentLoaded", function() {
-    formValidation();
+    sendSpiralDataToPlugin();
 });
 
 //initialize select menu
-selectMenu.init();
+// from  Figma Plugin DS UI library
+// selectMenu.init();
 
 //event listeners
-countInput.oninput = () => { formValidation(); }
-shapeMenu.onchange = () => { formValidation(); }
-createShapesButton.onclick = () => { createShapes(); }
-cancelButton.onclick = () => { cancel(); }
+// countInput.oninput = () => { formValidation(); }
+// shapeMenu.onchange = () => { formValidation(); }
+
+updateSpiralButton.onclick = () => { sendSpiralDataToPlugin(); }
+// updateSpiralButton.onclick = () => { createShapes(); }
+
+// cancelButton.onclick = () => { cancel(); }
 
 //form validation
 var formValidation = function(event) {
 
-    if (shapeMenu.value === '' || countInput.value === '') {
-        createShapesButton.disabled = true;
-    } else {
-        createShapesButton.disabled = false;
-    }
+    // if (shapeMenu.value === '' || countInput.value === '') {
+    //     createShapesButton.disabled = true;
+    // } else {
+    //     createShapesButton.disabled = false;
+    // }
 }
 
 
@@ -34,11 +36,278 @@ var formValidation = function(event) {
 function createShapes() {
     parent.postMessage({ pluginMessage: { 
         'type': 'create-shapes', 
-        'count': countInput.value,
-        'shape': shapeMenu.value
     } }, '*');
 }
 
 function cancel() {
     parent.postMessage({ pluginMessage: { 'type': 'cancel' } }, '*')
 }
+
+
+
+
+
+
+
+
+
+// Commented out so that it's possible to Right Click -> Inspect Element.
+// Disable the context menu
+// document.addEventListener("contextmenu", function(e) {
+//     e.preventDefault();
+//   });
+  
+  const SPIRAL_CONSTANTS = {
+    SPIRAL_TYPE_ARCHIMEDEAN: 0,
+    SPIRAL_TYPE_LOGARITHIMIC: 1
+  };
+  
+  var debugLogFlag = true;
+  
+  
+  var currentSpiralType = SPIRAL_CONSTANTS.SPIRAL_TYPE_ARCHIMEDEAN;
+  
+  var spiralType = document.getElementById('spiralType')
+  var innerR = document.getElementById('InnerRadius');
+  var outerR = document.getElementById('OuterRadius');
+  var degrees = document.getElementById('Degrees');
+  var rotations = document.getElementById('Rotations');
+  var points = document.getElementById('Points');
+  var lineWidth = document.getElementById('lineWidth');
+  var spiralGapLabel = document.getElementById('SpiralGapLabel');
+  var degreeIncrementLabel = document.getElementById('DegreeIncrementLabel');
+  var minLogRadiusWarnLabel = document.getElementById('minLogRadiusWarnLabel');
+  var minArchimedeanRadiusWarnLabel = document.getElementById('minArchimedeanRadiusWarnLabel');
+  
+  var shouldMakeHelixCheckbox = document.getElementById('helixCheckbox');
+  var helixAdjustHeightCheckbox = document.getElementById('helixAdjustHeightCheckbox');
+  var helixOffsetX = document.getElementById('xOffset');
+  var helixOffsetY = document.getElementById('yOffset');
+  var helixHWRatio = document.getElementById('helixHWRatio');
+  var helixIsoAngle = document.getElementById('helixIsoAngle');
+  
+  var continouslyUpdateCheckbox = document.getElementById('continouslyUpdateCheckbox');
+//   var updateSpiralButton = document.getElementById('updateSpiralButton');
+  var closePanelButton = document.getElementById('closePanel');
+  closePanelButton.onclick = () => {
+    parent.postMessage({ pluginMessage: { 
+        'type': 'done', 
+    } }, '*');
+   }
+  
+  
+  var minPointsText = "";
+  var minDegreesText = "";
+  
+  
+  spiralType.addEventListener('change', function(evt) {
+    if (this.value == "Archimedean Spiral") {
+      currentSpiralType = SPIRAL_CONSTANTS.SPIRAL_TYPE_ARCHIMEDEAN;
+    } else if (this.value == "Logarithmic Spiral") {
+      currentSpiralType = SPIRAL_CONSTANTS.SPIRAL_TYPE_LOGARITHIMIC;
+    }
+    setSpiralGapLabel();
+    setSpiralMinRadiusWarningLabels();
+    if(continouslyUpdateCheckbox.checked) {
+      updateSpiral();
+    }
+  });
+  
+  
+  innerR.addEventListener('input', function (evt) {
+    setSpiralGapLabel();
+    setSpiralMinRadiusWarningLabels();
+    if(continouslyUpdateCheckbox.checked) {
+      updateSpiral();
+    }
+  });
+  
+  outerR.addEventListener('input', function (evt) {
+    setSpiralGapLabel();
+    setSpiralMinRadiusWarningLabels();
+    if(continouslyUpdateCheckbox.checked) {
+      updateSpiral();
+    }
+  });
+  
+  rotations.addEventListener('input', function (evt) {
+    debugLog(this.value);
+    degrees.value = this.value*360;
+    if(degrees.value < 1.0) {
+      minDegreesText = "(min rotation is 1°)";
+    } else {
+      minDegreesText = "";
+    }
+  
+    setDegreeIncrementLabel();
+    setSpiralGapLabel();
+    if(continouslyUpdateCheckbox.checked) {
+      updateSpiral();
+    }
+  });
+  
+  degrees.addEventListener('input', function (evt) {
+    debugLog(this.value);
+    rotations.value = this.value/360;
+    if(degrees.value < 1.0) {
+      minDegreesText = " - USING ROTATION of 1°";
+    } else {
+      minDegreesText = "";
+    }
+    setDegreeIncrementLabel();
+    setSpiralGapLabel();
+    if(continouslyUpdateCheckbox.checked) {
+      updateSpiral();
+    }
+  });
+  
+  points.addEventListener('input', function (evt) {
+    debugLog(this.value);
+    if(this.value<2) {
+      minPointsText = " - USING 2 POINTS";
+    } else {
+      minPointsText = "";
+    }
+  
+    setDegreeIncrementLabel();
+    if(continouslyUpdateCheckbox.checked) {
+      updateSpiral();
+    }
+  });
+  
+  lineWidth.addEventListener('input', function (evt) {
+    if(continouslyUpdateCheckbox.checked) {
+      updateSpiral();
+    }
+  });
+  
+  continouslyUpdateCheckbox.addEventListener('click', function (evt) {
+    if(continouslyUpdateCheckbox.checked) {
+      updateSpiralButton.disabled = true;
+      updateSpiralButton.classList.add("disabled");
+      debugLog("Button disabled");
+      updateSpiral();
+    } else {
+      updateSpiralButton.disabled = false;
+      updateSpiralButton.classList.remove("disabled");
+      debugLog("Button enabled");
+    }
+  });
+  
+  helixOffsetX.addEventListener('input', function (evt) {
+    if(continouslyUpdateCheckbox.checked) {
+      updateSpiral();
+    }
+  });
+  
+  helixOffsetY.addEventListener('input', function (evt) {
+    if(continouslyUpdateCheckbox.checked) {
+      updateSpiral();
+    }
+  });
+  
+  helixHWRatio.addEventListener('input', function (evt) {
+    debugLog(this.value);
+    helixIsoAngle.value = ((Math.acos(this.value))*(180 / Math.PI)).toFixed(1);
+    if(continouslyUpdateCheckbox.checked) {
+      updateSpiral();
+    }
+  });
+  
+  helixIsoAngle.addEventListener('input', function (evt) {
+    debugLog(this.value);
+    helixHWRatio.value = Math.cos(this.value * (Math.PI / 180)).toFixed(3);
+    if(continouslyUpdateCheckbox.checked) {
+      updateSpiral();
+    }
+  });
+  
+//   updateSpiralButton.addEventListener('click',function(){
+//     debugLog('SPIRAL DEBUG // Spiral Button Clicked');
+//     updateSpiral();
+//     return false;
+//   });
+  
+  shouldMakeHelixCheckbox.addEventListener('click', function (evt) {
+    if(continouslyUpdateCheckbox.checked) {
+      updateSpiral();
+    }
+  });
+  
+  helixAdjustHeightCheckbox.addEventListener('click', function (evt) {
+    if(continouslyUpdateCheckbox.checked) {
+      updateSpiral();
+    }
+  });
+  
+  
+  function setDegreeIncrementLabel() {
+    var degreeIncrement = degrees.value/(Math.floor(points.value));
+    degreeIncrementLabel.innerHTML = "Point every " + degreeIncrement.toFixed(2) + " degrees" + minPointsText + minDegreesText;;
+  }
+  
+  function setSpiralGapLabel() {
+    if (currentSpiralType == SPIRAL_CONSTANTS.SPIRAL_TYPE_ARCHIMEDEAN) {
+      var spiralGap = Math.abs(outerR.value - innerR.value) / rotations.value;
+      spiralGapLabel.innerHTML = "Gap of " + spiralGap.toFixed(2) + " after each rotation of the spiral";
+    }
+    else if (currentSpiralType == SPIRAL_CONSTANTS.SPIRAL_TYPE_LOGARITHIMIC) {
+      var log_spiral_param_a = parseFloat(innerR.value);
+      var log_spiral_param_b = (Math.log(outerR.value/innerR.value))/(2*Math.PI*rotations.value);
+      spiralGapLabel.innerHTML = "Log spiral equation: a*e^(b*theta) -> a=" + log_spiral_param_a + "  theta=" + log_spiral_param_b.toFixed(4);
+    }
+  }
+  
+  function setSpiralMinRadiusWarningLabels() {
+    if(currentSpiralType == SPIRAL_CONSTANTS.SPIRAL_TYPE_LOGARITHIMIC) {
+      if(Math.round(innerR.value)<1.0 || Math.round(outerR.value)<1.0) {
+        minLogRadiusWarnLabel.hidden = false;
+      } else {
+        minLogRadiusWarnLabel.hidden = true;
+      }
+    } else {
+      minLogRadiusWarnLabel.hidden = true;
+  
+      if (innerR.value == 0.0 && outerR.value == 0.0) {
+        minArchimedeanRadiusWarnLabel.hidden = false;
+      } else {
+        minArchimedeanRadiusWarnLabel.hidden = true;
+      }
+  
+    }
+  }
+  
+  function updateSpiral() {
+    sendSpiralDataToPlugin()
+  }
+  
+  function sendSpiralDataToPlugin() {
+    // Create JSON object with the action we want to trigger and the current UNIX date
+    var data = {
+      "currentSpiralType": currentSpiralType,
+      "innerRadius": Math.round(innerR.value),
+      "outerRadius": Math.round(outerR.value),
+      "degrees": Math.round(degrees.value),
+      "points": Math.round(points.value),
+      "lineWidth": lineWidth.value,
+      "shouldMakeHelix": shouldMakeHelixCheckbox.checked,
+      "shouldAdjustHelixHeight": helixAdjustHeightCheckbox.checked,
+      "helixOffsetX": helixOffsetX.value,
+      "helixOffsetY": helixOffsetY.value,
+      "helixHWRatio": helixHWRatio.value,
+      "helixIsoAngle": helixIsoAngle.value,
+      "date": new Date().getTime()
+    }
+    debugLog(data);
+    
+    parent.postMessage({ pluginMessage: { 
+        'type': 'create-spiral', data
+    } }, '*');
+  }
+  
+  
+  function debugLog(msg) {
+    if (debugLogFlag) {
+      console.log(msg);
+    }
+  }
